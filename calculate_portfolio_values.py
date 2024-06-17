4039bb64e81b6c53872c88c35ecbbb2672a000ad
Chitra Vadlamani <cvadlamani@quantumcomputinginc.com>
2024-06-17 15:54:36 -0400

    update

--- calculate_portfolio_values.py
+++ calculate_portfolio_values.py
@@ -1,5 +1,3 @@
-
-
 def calculate_portfolio_values():
     import sys 
     import datetime
@@ -12,7 +10,7 @@ def calculate_portfolio_values():
     warnings.filterwarnings("ignore")
     
     # Set params                                                                                  
-    INIT_PORT_VAL = 1000000.0
+    initial_portfolio_value = 1000000.0
     OUT_OF_SAMPLE_DAYS = 30
 
     MIN_DATE = pd.to_datetime("2022-01-01")
@@ -22,29 +20,29 @@ def calculate_portfolio_values():
     df["Date"] = pd.to_datetime([pd.Timestamp(timestamp).date() for timestamp in df["Date"]])
     df = df[(df["Date"] >= MIN_DATE) & (df["Date"] <= MAX_DATE)]
 
-    beg_port_val = INIT_PORT_VAL
+    beginning_portfolio_value = initial_portfolio_value
     df = df.sort_values("Date")
-    adj_dates = sorted(df["Date"].unique())
-    num_adj_dates = len(adj_dates)
+    unique_dates = sorted(df["Date"].unique())
+    no_of_unique_dates = len(unique_dates)
     dates = None
-    port_vals = None
+    portfolio_values = None
 
 
-    for i in range(num_adj_dates):
+    for i in range(no_of_unique_dates):
 
         print(
             "Processing adjustment date %s"
-            % pd.to_datetime(adj_dates[i]).strftime("%Y-%m-%d")
+            % pd.to_datetime(unique_dates[i]).strftime("%Y-%m-%d")
         )
 
-        beg_date = pd.to_datetime(adj_dates[i])
-        if i < num_adj_dates - 1:
-            end_date = pd.to_datetime(adj_dates[i + 1])
+        beginning_date = pd.to_datetime(unique_dates[i])
+        if i < no_of_unique_dates - 1:
+            end_date = pd.to_datetime(unique_dates[i + 1])
         else:
-            end_date = beg_date + datetime.timedelta(days=OUT_OF_SAMPLE_DAYS)
+            end_date = beginning_date + datetime.timedelta(days=OUT_OF_SAMPLE_DAYS)
 
-        tmp_df = df[df["Date"] == beg_date]
-        stocks = tmp_df["Stock"]
+        temp_df = df[df["Date"] == beginning_date]
+        stocks = temp_df["Stock"]
         stocks = list(set(stocks))
 
         stocks = list(set(stocks)  - {"FISV"})
@@ -52,8 +50,8 @@ def calculate_portfolio_values():
         if end_date > pd.to_datetime("2023-10-20"):
             stocks = list(set(stocks) - {"ATVI"})
 
-        all_dates = [beg_date]
-        date0 = beg_date
+        all_dates = [beginning_date]
+        date0 = beginning_date
         while date0 < end_date:
             date0 = date0 + datetime.timedelta(days=1)
             all_dates.append(date0)
@@ -62,10 +60,9 @@ def calculate_portfolio_values():
 
         for stock in stocks:
             stock_df = pd.read_csv("data/%s.csv" % stock)
-            #stock_df["Date"] = stock_df["Date"].astype("datetime64[ns]")
             stock_df["Date"] = pd.to_datetime([pd.Timestamp(timestamp).date() for timestamp in stock_df["Date"]])
             stock_df = stock_df[
-                (stock_df["Date"] >= beg_date) & (stock_df["Date"] <= end_date)
+                (stock_df["Date"] >= beginning_date) & (stock_df["Date"] <= end_date)
             ]
 
             if price_df is None:
@@ -76,8 +73,8 @@ def calculate_portfolio_values():
         price_df = price_df.fillna(method="ffill").fillna(method="bfill")
         price_df = price_df.sort_values("Date")
 
-        tmp_dates = np.array(price_df["Date"])
-        tmp_port_vals = np.zeros(shape=(price_df.shape[0]))
+        temp_dates = np.array(price_df["Date"])
+        temp_portfolio_values = np.zeros(shape=(price_df.shape[0]))
 
         assert price_df.shape[0] > 0
 
@@ -90,19 +87,19 @@ def calculate_portfolio_values():
                 print(stock)
                 print(price_df[["Date", stock]])
                 
-            assert beg_price > 0, "Error in data for %s" % stock # this assertion was failing, when I comment it out we get all NaN values
+            assert beg_price > 0, "Error in data for %s" % stock 
 
-            stock_count = stock_wt * beg_port_val / beg_price
-            tmp_port_vals += stock_count * prices
+            stock_count = stock_wt * beginning_portfolio_value / beg_price
+            temp_portfolio_values += stock_count * prices
 
         if dates is None:
-            dates = tmp_dates
+            dates = temp_dates
         else:
-            dates = np.concatenate([dates, tmp_dates])
+            dates = np.concatenate([dates, temp_dates])
 
-        if port_vals is None:
-            port_vals = tmp_port_vals
+        if portfolio_values is None:
+            portfolio_values = temp_portfolio_values
         else:
-            port_vals = np.concatenate([port_vals, tmp_port_vals])
+            portfolio_values = np.concatenate([portfolio_values, temp_portfolio_values])
 
-        beg_port_val = port_vals[-1]
+        beginning_portfolio_value = portfolio_values[-1]
